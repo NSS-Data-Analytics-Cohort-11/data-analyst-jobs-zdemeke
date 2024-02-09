3bfa52963fb772d2a18a8c6fc27b0b73b9737b8c
zdemeke <dzenash@gmail.com>
2024-02-08 21:25:01 -0600

    All question

--- script/S1_script.sql
+++ script/S1_script.sql
@@ -55,4 +55,14 @@ WHERE review_count > 5000
 GROUP BY company
 ORDER BY AVG (star_rating) DESC;
 --Answer: General motors- 4.199999809
+--Q11: Find all the job titles that contain the word 'Analyst. How many different job titles are there?
+SELECT COUNT (title)
+FROM indeed_analyst_jobs
+WHERE title LIKE '%Analyst%'
+--Answer: 1636
+--Q12. How many different job titles do not contain either the word 'Analyst' or the word 'Analytics'? What word do these positions have in common?
+SELECT count (title)
+FROM indeed_analyst_jobs
+WHERE title NOT LIKE '%Analyst%'OR title NOT LIKE '%Analytics%'
+__Answer: 1687
 
